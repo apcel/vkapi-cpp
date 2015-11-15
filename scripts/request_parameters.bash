279e6277f5cd1001dbf34662756655dba9453d25
apcel <apcel@yandex.ru>
2015-11-15 16:40:52 +0300

    New version of generator-scripts Automatically changes 'long' parameter name to 'long_' and so on Added comparisons to form request parameters

--- scripts/request_parameters.bash
+++ scripts/request_parameters.bash
@@ -4,7 +4,12 @@ METHOD_NAME=`bash ./namehelper.bash $1`
 
 PARAMETERS=`perl -pe 's/^(.*?)\n/ \"&$1=\" + $1 + /' $1`
 PARAMETERS=`echo $PARAMETERS | perl -pe 's/^\"\&/\"/' | perl -pe 's/\s*\+\s*$//'`
+cat $1 |
+    perl -pe 's/^(.*?)\n/ if \(\($1 != \"\\0\"\)&&\($1 != \"\"\)\) {requestParameters += \(\"&$1=\" +  $1\);};/g'|
+    perl -pe 's/^(.*?&&.*?)&/$1/' | perl -pe 's/^(.*?)\+=/$1=/' > /tmp/asdfasdf.txt
+TEST_CMD=$(cat /tmp/asdfasdf.txt)
+TEST_CMD=`echo $TEST_CMD | perl -pe 's/([^&\"])long([^\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\"])/$1_$2/g'`
 # echo $PARAMETERS
 # PARAMETERS=`echo $PARAMETERS | perl -pe 's/(.*),\s*/$1/; $_=$1'`
-[ -z "$PARAMETERS" ] && REQUEST_CMD="request(\"$METHOD_NAME\");" || REQUEST_CMD="request(\"$METHOD_NAME\", $PARAMETERS);"
+[ -z "$PARAMETERS" ] && REQUEST_CMD="request(\"$METHOD_NAME\");" || REQUEST_CMD="std::string requestParameters; $TEST_CMD; request(\"$METHOD_NAME\", requestParameters);"
 echo -e " {\n\t$REQUEST_CMD\n}"
