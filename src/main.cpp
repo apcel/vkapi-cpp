ed589d14d4737b324cfa323de3d5c542ebad0942
apcel <apcel@yandex.ru>
2015-11-15 16:44:43 +0300

    Just for history (Do not use this commit!) New version of api added. I think, it would not work. Why do I commit this? >.<

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "https.h"
+#include "vkapi.h"
 int main(int argc, char* argv[])
 {
+    https * httpsObject = new https;
+
+    vkapi * vkapiObject = new vkapi(httpsObject);
+
+    // std::string message = std::string(argv[1]);
+    // std::cout << message << std::endl;
+
+    // vkapiObject->messagesSend("", "",  "apcel", "", "", "message", "",  "", "", "", "", "");
+    vkapiObject->save();
     return 0;
 }
