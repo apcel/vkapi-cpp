3cab3d62a3fe7f96a5f2f4e462d46ee9a61899f7
apcel <apcel@yandex.ru>
2015-11-15 22:29:25 +0300

    Updated request_parameters.bash Doing the same comparison twice wasn't a good idea.

--- scripts/request_parameters.bash
+++ scripts/request_parameters.bash
@@ -5,8 +5,8 @@ METHOD_NAME=`bash ./namehelper.bash $1`
 PARAMETERS=`perl -pe 's/^(.*?)\n/ \"&$1=\" + $1 + /' $1`
 PARAMETERS=`echo $PARAMETERS | perl -pe 's/^\"\&/\"/' | perl -pe 's/\s*\+\s*$//'`
 cat $1 |
-    perl -pe 's/^(.*?)\n/ if \(\($1 != \"\\0\"\)&&\($1 != \"\"\)\) {requestParameters \+= \(\"&$1=\" \+  httpsObject\-\>escape\($1\)\);};/g'|
-    perl -pe 's/^(.*?&&.*?)&/$1/' | perl -pe 's/^(.*?)\+=/$1=/' > /tmp/asdfasdf.txt
+    perl -pe 's/^(.*?)\n/ if \($1 != \"\"\) {requestParameters \+= \(\"&$1=\" \+  httpsObject\-\>escape\($1\)\);};/g'|
+    perl -pe 's/^(.*?)&/$1/' | perl -pe 's/^(.*?)\+=/$1=/' > /tmp/asdfasdf.txt
 TEST_CMD=$(cat /tmp/asdfasdf.txt)
 TEST_CMD=`echo $TEST_CMD | perl -pe 's/([^&\"])long([^\w\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\w\"])/$1_$2/g'`
 # echo $PARAMETERS
