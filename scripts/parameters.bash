3d16d9525b99d4b22a6a82373fe070ca819b9dba
apcel <apcel@yandex.ru>
2015-11-15 16:51:34 +0300

    New version of scripts Do not rename 'longtitude' to 'long_titude' and so on

--- scripts/parameters.bash
+++ scripts/parameters.bash
@@ -2,5 +2,5 @@
 #$1 is filename
 PARAMETERS=`perl -pe 's/^(.*?)\n/std::string $1, /' $1`
 # echo $PARAMETERS
-PARAMETERS=`echo $PARAMETERS | perl -pe 's/(.*),\s*/$1/; $_=$1' | perl -pe 's/([^&\"])long([^\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\"])/$1_$2/g'`
+PARAMETERS=`echo $PARAMETERS | perl -pe 's/(.*),\s*/$1/; $_=$1' | perl -pe 's/([^&\"])long([^\w\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\w\"])/$1_$2/g'`
 echo -n \($PARAMETERS\)
--- scripts/request_parameters.bash
+++ scripts/request_parameters.bash
@@ -8,7 +8,7 @@ cat $1 |
     perl -pe 's/^(.*?)\n/ if \(\($1 != \"\\0\"\)&&\($1 != \"\"\)\) {requestParameters += \(\"&$1=\" +  $1\);};/g'|
     perl -pe 's/^(.*?&&.*?)&/$1/' | perl -pe 's/^(.*?)\+=/$1=/' > /tmp/asdfasdf.txt
 TEST_CMD=$(cat /tmp/asdfasdf.txt)
-TEST_CMD=`echo $TEST_CMD | perl -pe 's/([^&\"])long([^\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\"])/$1_$2/g'`
+TEST_CMD=`echo $TEST_CMD | perl -pe 's/([^&\"])long([^\w\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\w\"])/$1_$2/g'`
 # echo $PARAMETERS
 # PARAMETERS=`echo $PARAMETERS | perl -pe 's/(.*),\s*/$1/; $_=$1'`
 [ -z "$PARAMETERS" ] && REQUEST_CMD="request(\"$METHOD_NAME\");" || REQUEST_CMD="std::string requestParameters; $TEST_CMD; request(\"$METHOD_NAME\", requestParameters);"
