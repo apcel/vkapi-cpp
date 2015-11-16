10d104337dd3240478724c0dd87b50d87b60ecda
apcel <apcel@yandex.ru>
2015-11-16 21:34:21 +0300

    Updated main.bash A little change to output format: * Now it makes multi-line output for each function (you still have to format code with, for example, AStyleFormatter) * It places a single-line comment between generated functions

--- scripts/main.bash
+++ scripts/main.bash
@@ -6,4 +6,5 @@ REQUEST_PARAMETERS=`bash ./request_parameters.bash $1`
 CPP=`bash ./structurator.bash $1 "vkapi::" "$REQUEST_PARAMETERS"`
 echo $HEADER >> $HEADER_OUTPUT
 # echo '=================================='
-echo $CPP >> $CPP_OUTPUT
+echo "/*************************************************************************/" >>  $CPP_OUTPUT
+echo $CPP | perl -pe ' s/;;/;/g; s/\sif\s/\nif /g; s/{/{\n/g; s/};/};\n/g; chomp;' >> $CPP_OUTPUT
