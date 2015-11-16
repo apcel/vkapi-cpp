95849370b7316a1fb81b5cc178ca48396067cfbf
apcel <apcel@yandex.ru>
2015-11-16 21:40:00 +0300

    Some changes to testing main.cpp Now it tests any number of api requests

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,13 @@ int main(int argc, char* argv[])
 
     // std::string message = std::string(argv[1]);
     // std::cout << message << std::endl;
-    vkapiObject->messagesSend("", "",  "apcel", "", "", "message", "",  "", "", "", "", "");
+    for (int i = 0; i < 3; ++i){
+        vkapiObject->messagesSend("", "",  "apcel", "", "", std::to_string(i), "",  "", "", "", "", "");
+        // system("sleep 1");
+    }
     vkapiObject->save();
+    // std::string a; std::cin >> a;
+    delete vkapiObject;
+    delete httpsObject;
     return 0;
 }
