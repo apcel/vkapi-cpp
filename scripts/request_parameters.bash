db63ccb758554f8e17454740dd572a334e3a05c2
apcel <apcel@yandex.ru>
2015-11-16 21:37:28 +0300

    Updated request_parameters.bash A minor code change

--- scripts/request_parameters.bash
+++ scripts/request_parameters.bash
@@ -6,10 +6,10 @@ PARAMETERS=`perl -pe 's/^(.*?)\n/ \"&$1=\" + $1 + /' $1`
 PARAMETERS=`echo $PARAMETERS | perl -pe 's/^\"\&/\"/' | perl -pe 's/\s*\+\s*$//'`
 cat $1 |
     perl -pe 's/^(.*?)\n/ if \($1 != \"\"\) {requestParameters \+= \(\"&$1=\" \+  httpsObject\-\>escape\($1\)\);};/g'|
-    perl -pe 's/^(.*?)&/$1/' | perl -pe 's/^(.*?)\+=/$1=/' > /tmp/asdfasdf.txt
+    perl -pe 's/^(.*?)&/$1/'> /tmp/asdfasdf.txt
 TEST_CMD=$(cat /tmp/asdfasdf.txt)
 TEST_CMD=`echo $TEST_CMD | perl -pe 's/([^&\"])long([^\w\"])/$1long_$2/g' | perl -pe 's/([^&\"]signed)([^\w\"])/$1_$2/g'`
 # echo $PARAMETERS
 # PARAMETERS=`echo $PARAMETERS | perl -pe 's/(.*),\s*/$1/; $_=$1'`
-[ -z "$PARAMETERS" ] && REQUEST_CMD="request(\"$METHOD_NAME\");" || REQUEST_CMD="std::string requestParameters; $TEST_CMD; request(\"$METHOD_NAME\", requestParameters);"
+[ -z "$PARAMETERS" ] && REQUEST_CMD="request(\"$METHOD_NAME\");" || REQUEST_CMD="std::string requestParameters = \"?\"; $TEST_CMD; request(\"$METHOD_NAME\", requestParameters);"
 echo -e " {\n\t$REQUEST_CMD\n}"
