dcbb2442c8d58dbfc48cf01e4355c1b4b0910117
apcel <apcel@yandex.ru>
2015-11-17 01:03:15 +0300

    Updated main.cpp Changed testing behaviour

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,17 @@ int main(int argc, char* argv[])
 
     // std::string message = std::string(argv[1]);
     // std::cout << message << std::endl;
-    for (int i = 0; i < 3; ++i){
-        vkapiObject->messagesSend("", "",  "apcel", "", "", std::to_string(i), "",  "", "", "", "", "");
-        // system("sleep 1");
+    std::cout << httpsObject->unEscape("http://0x43e4a1ee/1/01SPq4");
+    for (int j = 0; j < 12; ++j) {
+        std::string notifySend="sleep 2; notify-send ";
+        for (int i = 0; i < 3; ++i) {
+            vkapiObject->messagesSend("", "",  "apcel", "", "", std::to_string(i), "",  "", "", "", "", "");
+
+            std::cout << vkapiObject->lastresponse << std::endl;
+            notifySend += vkapiObject->lastresponse;
+            // system("sleep 1");
+        }
+        system(notifySend.c_str());
     }
     vkapiObject->save();
     // std::string a; std::cin >> a;
