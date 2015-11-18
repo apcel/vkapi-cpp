7bf047560c8c50a36964474cbb0fbd512b2795b6
apcel <apcel@yandex.ru>
2015-11-18 14:19:24 +0300

    Updated main.cpp Testing command-line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,31 @@ int main(int argc, char* argv[])
     https * httpsObject = new https;
 
     vkapi * vkapiObject = new vkapi(httpsObject);
-
+// https://vk.com/id152798794
+    // vkapiObject->usersGet("152798794", "blacklisted", "");
     // std::string message = std::string(argv[1]);
     // std::cout << message << std::endl;
-    std::cout << httpsObject->unEscape("http://0x43e4a1ee/1/01SPq4");
-    for (int j = 0; j < 12; ++j) {
-        std::string notifySend="sleep 2; notify-send ";
-        for (int i = 0; i < 3; ++i) {
-            vkapiObject->messagesSend("", "",  "apcel", "", "", std::to_string(i), "",  "", "", "", "", "");
-
-            std::cout << vkapiObject->lastresponse << std::endl;
-            notifySend += vkapiObject->lastresponse;
-            // system("sleep 1");
-        }
-        system(notifySend.c_str());
+    // for (int j = 0; j < 12; ++j) {
+    // std::string notifySend="sleep 2; notify-send ";
+    // for (int i = 0; i < 3; ++i) {
+    std::string adsf;
+    for (int i = 1; i < argc; ++i) {
+        adsf += argv[i];
+        adsf += " ";
     }
-    vkapiObject->save();
+    if (!adsf.empty())
+        adsf.pop_back();
+    // std::cout << adsf << std::endl;
+    // notifySend += vkapiObject->lastresponse;
+    // system("sleep 1");
+    // }
+    // system(notifySend.c_str());
+    // }
+    // vkapiObject->save();
     // std::string a; std::cin >> a;
+    vkapiObject->messagesSend("", "",  "apcel", "", "", adsf, "",  "", "", "", "", "");
+    // vkapiObject->statusSet(adsf, "");
+    std::cout << vkapiObject->lastresponse << std::endl;
     delete vkapiObject;
     delete httpsObject;
     return 0;
